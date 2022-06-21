ce7156590211d3ac0a8367cedecf08797b378acc
Arthur Lapz <rlapz@gnuweeb.org>
2022-06-21 09:20:06 +0700

    neko.py: Send appropriate file type

--- userbot/commands/neko.py
+++ userbot/commands/neko.py
@@ -7,28 +7,34 @@ import requests
 # Usage: neko [-h|ARG]
 @app.on_message(filters.command("neko", prefixes=".") & filters.me)
 async def getNeko(app, msg):
-    _ = app
     url_api = "http://api.nekos.fun:8080/api/"
     cmd = msg.command
 
-    if (len(cmd) == 1):
+    if len(cmd) == 1:
         cmd = cmd[0]
     else:
         cmd = cmd[1]
-        if (cmd == "-h"):
+        if cmd == "-h":
             await msg.reply("https://www.nekos.fun/apidoc.html")
             return
 
     try:
         res = requests.get(url_api + cmd).text
         img = json.loads(res)["image"]
-        rep = msg.reply_to_message
+        ext = img.split(".")[-1]
 
-        if (rep):
-            await msg.reply(img, reply_to_message_id=rep.id)
+        rep = msg.from_user.id
+        if msg.reply_to_message:
+            rep = msg.reply_to_message.id
+
+        if ext == "gif":
+            await app.send_animation(msg.chat.id, animation=img,
+                                     reply_to_message_id=rep)
         else:
-            await msg.edit_text(img)
-    except:
-        pass
+            await app.send_photo(msg.chat.id, photo=img,
+                                 reply_to_message_id=rep)
+    # Dumb exception handler :v
+    except Exception as e:
+        print(e)
 
 print("neko.py has been loaded")
