b1cce1dab2ca4278b64209dca1ac5f0a0860be5e
Arthur Lapz <rlapz@gnuweeb.org>
2022-06-21 19:47:14 +0700

    commands: Add `commands` command
    Show command lists

--- userbot/commands/basic.py
+++ userbot/commands/basic.py
@@ -1,5 +1,6 @@
-from userbot import app
+from userbot import app, commands
 from pyrogram import filters
+import json
 
 
 # Usage: info
@@ -12,4 +13,14 @@ async def getInfo(app, msg):
     else:
         await msg.reply(f"```{msg.from_user}```")
 
+# Usage: modules
+@app.on_message(filters.command("commands", prefixes=".") & filters.me)
+async def getCommands(app, msg):
+    _ = app
+
+    res = json.dumps(commands, sort_keys=True, indent=4)
+    await msg.reply(f"```{res}```")
+
+
+commands["basic.py"] = [ "info", "commands" ]
 print("basic.py has been loaded")
